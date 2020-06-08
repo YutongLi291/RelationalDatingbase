fd1f36bc3f93607b4bb88d5f5f8d3d4d41ce0274
ahaniesa <madmattcrazy@gmail.com>
2020-06-07 22:12:34 -0700

    sign in page done and template

--- signin.php
+++ signin.php
@@ -1,26 +1,70 @@
-<form action="saveagestatus.php" method="post">
 <?php
-include 'connect.php';
-$conn = OpenCon();
-$sql = "SELECT * from agestatus";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-	echo "<table><tr><th class='border-
-	class'>Age</th><th class='border-class'>Adult? name</th></tr>";// output data of each row
-	while($row = 
-	$result->fetch_assoc()) { echo "<tr><td class='border-class'>".$row["age"]."</td><td 
-class='border-class'>".$row["isAdult"]."</td></tr>";}echo "</table>";} else {echo "0 
-results";}
-CloseCon($conn);
+session_start();
 ?>
-<h1> ENTER A NEW AGE STATUS (For testing only) </h1>
-	<label>Age</label>
-    <input name="age" type="text" placeholder="Age">
-    <br>
-    <label>Adult</label>
-    <input name="adult" type="text" placeholder="Adult?">
-        <br>
-        <br>
-    <input type="submit" value="Add value">
-</form>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>SIGN IN</title>
+		<link rel="stylesheet" href = "styles.css">
+		<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet">
+	</head>
+	<body>
+
+		<?php
+		include 'connect.php';
+		$conn = OpenCon();
+		$sql = "SELECT * from users";
+		$result = $conn->query($sql);
+
+		// if ($result->num_rows > 0) {
+		// 	echo "<table>
+		// 	<tr>
+		// 	<th class='border-class'>First Name</th>
+		// 	<th class='border-class'>Password</th>
+		// 	</tr>";// output data of each row
+
+		// 	while($row = 
+		// 	$result->fetch_assoc()) { 
+		// 		echo "<tr><td class='border-class'>".$row["firstName"]."</td>
+		// 		<td class='border-class'>".$row["password"]."</td></tr>";}echo "</table>";} else {echo "0 results";
+		// 	}
+
+		// 	CloseCon($conn);
+		?>
+
+		<div id="signinpage">
+			<div id="signincover">
+				<br>
+				<h1> Relational-Datingbase</h1>
+				
+				<form id="signinform" onsubmit="" action="signin_check.php" method="post">
+					<p id="signintitle"> SIGN IN </p><br>
+					<label>Username</label><br>
+				    <input name="username" type="text" placeholder="Email address">
+				    <br>
+				    <label>Password</label><br>
+				    <input name="password" type="text" placeholder="Password">
+				        <br>
+				        <br>
+				    <button type="submit" name="signinbutton" value="submit" id="signinbutton"> SIGN IN</button>
+				    <br><br>
+				    <?php
+		                if(isset($_SESSION["error"])){
+		                    $error = $_SESSION["error"];
+		                    echo "<span>$error</span>";
+		                }
+		            ?>  
+		            <br><br><br>
+		            <p> Don't have an account yet? <a href="signup.php" id="signinsignup">SIGN UP</a></p>
+				</form>
+			<div>
+		</div>
+	</body>
+</html>
+
+<?php
+    unset($_SESSION["error"]);
+?>
---
+++ signin_check.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+include 'connect.php';
+$conn = OpenCon();
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+$error = "Username/password is incorrect";
+
+// if(isset($_POST['signinbutton'])){	THIS IS JUST TO TEST IF YOU ARE GRABBING THE RIGHT USERNAME AND PASSWORD VALUE
+// 	echo $username." ".$password;
+// }
+
+$query = "SELECT * FROM users WHERE email = '". $username ."' AND password = '". $password ."'" ;
+$result = $conn->query($query);
+
+//echo mysqli_num_rows($result); TO TEST THE NUMBER OF ROWS OUTPUTTED
+
+if (mysqli_num_rows($result) == 1) {
+	$_SESSION["username"] = $username;
+	header("Location: homefeed.php");
+} else {
+	$_SESSION["error"] = $error;
+    header("location: signin.php");
+}
+
+?>
