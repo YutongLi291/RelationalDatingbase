d1db1a52bf3f3e0112a45e8474e4dd00ef9bcd26
samanthahsu <shsu877@gmail.com>
2020-06-09 16:39:01 -0700

    isolated primitive messaging working

--- display_chat.php
+++ display_chat.php
@@ -1,61 +1,101 @@
 <?php
-    session_start();
-    $_SESSION['userEmail'] = "bbbb@hotmail.com";
-    $_SESSION['cID'] = 5;
-    // so we know who's logged in. We already know the conversation.
-
     include 'connect.php';
-    $conn = OpenCon();
-    $currUser = $_SESSION['userEmail'];
-    $currCID = $_SESSION['cID'];
+    include "save_message.php";
 
-    // get name of other user
-    $sql = 
-        "SELECT
-            firstname,
-            lastname,
-            u.email
-        FROM
-            users AS u,
-            usermatchescontains AS umc
-        WHERE
-            u.email != '$currUser' AND umc.cID = $currCID AND(
-                u.email = umc.firstUser OR u.email = umc.secondUser
-            )";
-    $result = $conn->query($sql);
-    if ($row = $result->fetch_assoc()) {
-        $partnerName = $row["firstname"]." ".$row["lastname"];
-    } else {
-        echo "error";
+    if (!isset($_SESSION['userEmail'])) {
+        session_start();
+        setCurrCID(5);
+        setCurrUser("bbbbra@hotmail.com");
+    } 
+
+    if (isset($_POST['loginAs'])) setCurrUser($_POST['loginAs']);
+    if (isset($_POST['cID'])) setCurrCID($_POST['cID']);
+
+    if (isset($_POST['text'])) {
+        send();
+        unset($_POST['text']);
     }
+    echo "You are logged in as ".$_SESSION['userEmail']." in conversation cID ".$_SESSION['cID'];
 
-    $sql = "SELECT * from hasusermessages where userEmail='$currUser'";
-    //  where userEmail=$currUser and cId=$currCID order by timeSent
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<table><tr><th class='border-class'>Messaging</th>
-        <tr>
-        <tr><th class='border-class'>User</th>
-        <th class='border-class'>TimeSent</th>
-        <th class='border-class'>Message</th></tr>";// output data of each row
-        while($row = 
-        $result->fetch_assoc()) { 
-            if ($row["userEmail"] = $_SESSION["userEmail"]) {
-                $sender = "you";
-            } else {
-                $sender = $partnerName;
-            }
-            echo "<tr><td class='border-class'>".$sender."</td><td 
-            class='border-class'>".$row["timeSent"]."</td><td 
-            class='border-class'>".$row["text"]."</td></tr>";
-        }
-        echo "</table>";
-    } else {echo "no messages yet... say hi :)";}
-    CloseCon($conn);
+    start_chat();
 ?>
 
-<form action="conversation.php" method="post">
-        Type Here: <input type="text" name="text" size="100" placeholder="say something nice">
-        <input type="submit">
+<?php
+    function setCurrCID($currCID) {
+        $_SESSION['cID'] = $currCID;
+    }
+    function setCurrUser($currUser) {
+        $_SESSION['userEmail'] = $currUser;
+    }
+
+    function start_chat() {
+    
+        $conn = OpenCon();
+        $currUser = $_SESSION['userEmail'];
+        $currCID = $_SESSION['cID'];
+    
+        // get name of other user
+        $sql = 
+            "SELECT
+                firstname,
+                lastname,
+                u.email
+            FROM
+                users AS u,
+                usermatchescontains AS umc
+            WHERE
+                u.email != '$currUser' AND umc.cID = $currCID AND(
+                    u.email = umc.firstUser OR u.email = umc.secondUser
+                )";
+        $result = $conn->query($sql);
+        if ($row = $result->fetch_assoc()) {
+            $partnerName = $row["firstname"]." ".$row["lastname"];
+        } else {
+            echo "error";
+        }
+    
+        $sql = "SELECT * from hasusermessages where cID=$currCID order by timeSent";
+        //  where userEmail=$currUser and cId=$currCID order by timeSent
+        $result = $conn->query($sql);
+    
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th class='border-class'>Messaging</th></tr>
+                <tr><th class='border-class'>User</th>
+                <th class='border-class'>TimeSent</th>
+                <th class='border-class'>Message</th>
+            </tr>"; // output data of each row
+            while($row = 
+            $result->fetch_assoc()) { 
+                if ($row["userEmail"] == $currUser) {
+                    $sender = "you";
+                } else {
+                    $sender = $partnerName;
+                }
+                echo "<tr><td class='border-class'>".$sender."</td><td 
+                class='border-class'>".$row["timeSent"]."</td><td 
+                class='border-class'>".$row["text"]."</td></tr>";
+            }
+            echo "</table>";
+        } else {echo "no messages yet... say hi :)";}
+        CloseCon($conn);
+        }
+?>
+
+
+<?php
+    function postMessage() {
+        // code to be executed;      
+        echo 'posted!';
+    }
+?>
+
+<form method="POST" onAction="display_chat.php">
+    Type Here: <input type="text" name="text" size="100" placeholder="something nice">
+    <input type="submit" name="send" onClick>
+</form>
+
+<form method="POST" onAction="display_chat.php">
+    Your email: <input type="text" name="loginAs" placeholder="bbbbra@hotmail.com">
+    cID: <input type="text" name="cID"  placeholder="5">
+    <input type="submit" name="send" onClick>
 </form>
--- save_message.php
+++ save_message.php
@@ -1,35 +1,47 @@
 <?php
-function run($obConn,$sql)
-{
-	$rsResult = mysqli_query($obConn, $sql) or die(mysqli_error($obConn));
-}
+    include_once 'connect.php';
+    session_start();
 
-// find messageID of next message to send
-$currCID = $_SESSION["cID"];
-if (isset($_SESSION["meID"])) {
-    $_SESSION["meID"] += 1;
-} else {
-    // this is only run if this is the first message the user is sending for the time they've been in this conversation on this site.
-    $sql =
-    "SELECT meID
-    from hasusermessages
-    where cID=$currCID
-    order by meID DESC";
-
-    $result = $conn->query($sql);
-    if ($row = $result->fetch_assoc()) {
-        $_SESSION["meID"] = $row["meID"] + 1;
-    } else {
-        $_SESSION["meID"] = 1;
+    function run($obConn,$sql)
+    {
+        $rsResult = mysqli_query($obConn, $sql) or die(mysqli_error($obConn));
     }
-}
 
-include'connect.php';
-$currUser = $_SESSION["userEmail"];
-$currCID = $_SESSION["cID"];
-$currMeID = $_SESSION["meID"];
-$text = $_POST['text'];
-$conn = OpenCon();
-$sql = "INSERT INTO hasusermessages VALUES($currMeID, 'current time', $text, '$currUser', '$currCID'";
-run($conn,$sql);
+// pre: $_POST['text'] is set.
+    function send() {
+        $conn = OpenCon();
+        // find messageID of next message to send
+        $currCID = $_SESSION['cID'];
+        if (isset($_SESSION["meID"])) {
+            $_SESSION["meID"] += 1;
+        } else {
+            // this is only run if this is the first message the user is sending for the time they've been in this conversation on this site.
+            $sql =
+            "SELECT meID
+            from hasusermessages
+            where cID = $currCID
+            order by meID DESC";
+
+            $result = $conn->query($sql);
+            if ($row = $result->fetch_assoc()) {
+                $_SESSION["meID"] = $row["meID"] + 1;
+            } else {
+                $_SESSION["meID"] = 1;
+            }
+        }
+
+        $currUser = $_SESSION["userEmail"];
+        $currCID = $_SESSION["cID"];
+        $currMeID = $_SESSION["meID"];
+        $text = $_POST['text'];
+
+        // gets current time
+        $sql = "SELECT NOW()";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $currTime = $row['NOW()'];
+
+        $sql = "INSERT INTO hasusermessages (meID, timeSent, text, userEmail, cID) VALUES ($currMeID, '$currTime', '$text', '$currUser', $currCID);";
+        run($conn,$sql);
+    }
 ?>
