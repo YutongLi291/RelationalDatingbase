9ea767e5e344c4794b586e7bddf331e6aa3caeb6
samanthahsu <shsu877@gmail.com>
2020-06-17 08:19:57 -0700

    having fun with stats

--- stats.php
+++ stats.php
@@ -9,9 +9,10 @@
 	</head>
 	<body>
 
-	<h3><br><br><br>Are you taking CPSC304? <p> LIKE A NERD?! </h3>
-	<h1><br>Here There Be Database Secret Info</h1>
-
+	<h1><br><br><br><br>Congrats, You Found This Page!</h1>
+	<h3 class="center"><br><br><br><br><br>...<br><br><br><br>Are you taking CPSC304?</h3>
+	<h1><br><br><br><br><br><br><br>LIKE A NERD?!<br><br></h1>
+	<h3 class="center"><br><br>Here There Be Database Secret Info<br>-----------------------</h3>
 		<?php
 			include 'connect.php';
 			include 'header.php';
@@ -21,7 +22,7 @@
 			$sql = "SELECT count(*) from users";
             $result = $conn->query($sql);
             $numUsers = $result->fetch_assoc()['count(*)'];
-            echo "<p>Number of Users Registered: {$numUsers}</p>";
+            echo "<p class=\"center\">Number of Users Registered: {$numUsers}</p>";
 			
 			//FIND USER With the MOST MATCHES
 			$sql = 
@@ -38,16 +39,16 @@
 			";
 			$result = $conn->query($sql);
 			if ($topuser = $result->fetch_assoc()) {
-				echo "User with the most matches: ".$topuser['firstName']." ".$topuser['lastName']." (Watch out for this person)";
+				echo "<p class=\"center\">User with the most matches: ".$topuser['firstName']." ".$topuser['lastName']." (Watch out for this person)";
 			} else {
-				echo "There is no top user at the moment!";
+				echo "<p class=\"center\">There is no top user at the moment!";
 			}
 			
 			///NUMBER OF TOTAL MESSAGES SENT
 			$sql = "SELECT count(*) as count from hasusermessages";
 			$result = $conn->query($sql);
 			$totalmessages = $result->fetch_assoc();
-			echo "<p>Total number of messages sent: " .$totalmessages['count']. "</p>";
+			echo "<p class=\"center\">Total number of messages sent: " .$totalmessages['count']. "</p>";
 			
 			 /// AVERAGE number of MATCHES found
 			$sql = "SELECT avg(totalMatches) as average from (
@@ -75,12 +76,12 @@
             ) as allUsers";
 		   $result = $conn->query($sql);
 		   $avgmatches = $result->fetch_assoc();
-		   echo "<p>Average number of matches found for each user: " .$avgmatches['average']. "</p>";
+		   echo "<p class=\"center\">Average number of matches found for each user: " .$avgmatches['average']. "</p>";
 
 		?>
 
 
-		<h1>Find users who have been to (posted in) these locations:</h1>
+		<h3>Find users who have been to (posted in) these locations:</h3>
 		<!-- users who have posted at all the chosen locations! -->
 
 		<form method="POST" action="find_user_location_post.php">
@@ -91,7 +92,7 @@
 				$curr_ID=$row['locID'];
 				$curr_province=$row['province'];
 				$curr_city=$row['city'];
-				echo "<input type=\"checkbox\" name={$curr_ID} >
+				echo "<input type=\"checkbox\" name={$curr_ID}>
 				<label for={$curr_ID}>{$curr_city} {$curr_province}</label><br>";
 			}
 			?>
@@ -103,16 +104,16 @@
 			<button name="button" type="submit">Submit</button>
 		</form>
 
-		<p>
+		<h3 class="center">
+			Results:<br>-----------<br>
 			<?php
 				if (isset($_SESSION['loc_users'])) {
 					echo $_SESSION['loc_users'];
-					// unset($_SESSION['loc_users']);
 				} else {
 					echo "no users fit the requirements, please select something different:";
 				} 
 				?>
-		</p>
+		</h3>
 		
 	</body>
 </html>
